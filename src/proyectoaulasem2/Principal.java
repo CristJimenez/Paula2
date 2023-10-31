c89999f98cd5103f94498612b61c74eb007a0c61
CristJimenez <123cristianjose@gmail.com>
2023-10-31 08:27:58 -0500

    Primer commit

---
+++ src/proyectoaulasem2/Principal.java
@@ -0,0 +1,17 @@
+
+package proyectoaulasem2;
+
+import proyectoaulasem2.ventanas.VentanaPrincipal;
+
+/**
+ *
+ * @author 123cr
+ */
+public class Principal {
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setExtendedState(VentanaPrincipal.MAXIMIZED_BOTH);
+        ventana.setTitle("Sistema de Ventas");
+        ventana.setVisible(true);
+    }
+}
---
+++ src/proyectoaulasem2/datos/Producto.java
@@ -0,0 +1,12 @@
+
+package proyectoaulasem2.datos;
+
+/**
+ *
+ * @author 123cr
+ */
+public class Producto {
+    public String codigo;
+    public String nombre;
+    public double precio;
+}
